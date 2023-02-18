8e8d243711f6900f2798afe118b27d93037ac35b
Phan Mỹ Linh <havana03022003@gmail.com>
2023-02-19 02:12:27 +0700

    .

--- app/src/main/java/com/example/a5androidtrivia/GameFragment.kt
+++ app/src/main/java/com/example/a5androidtrivia/GameFragment.kt
@@ -5,21 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [GameFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GameFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
     data class Question(val text: String, val answers:List<String>)
 
     private val question: MutableList<Question> = mutableListOf(
@@ -49,20 +39,16 @@ class GameFragment : Fragment() {
     private var questionIndex = 0
     private val numQuestions = Math.min((question.size + 1) / 2, 3)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        val view : View = inflater.inflate(R.layout.fragment_game, container, false)
+        randomizeQuestions()
+
+        return view
     }
 
     // randomize the questions and set the first question
@@ -80,25 +66,7 @@ class GameFragment : Fragment() {
         answers = currentQuestion.answers.toMutableList()
         // and shuffle them
         answers.shuffle()
-//        (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.title_android_trivia_question, questionIndex + 1, numQuestions)
+//        (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.app_name, questionIndex + 1, numQuestions)
     }
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GameFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GameFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
